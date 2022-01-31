0373999f9c4d0a50ec8aa32c6f7a8ccbc5c7172b
Elea Luzi <85105911+Ele-nonsochi@users.noreply.github.com>
2022-01-31 16:37:10 +0100

    Start upload img

--- app/User.php
+++ app/User.php
@@ -37,7 +37,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
     
-    //Relazione one to many
+    //Relazione one to many hvgo'
     public function posts()
     {
         return $this->hasMany('App\Post');
